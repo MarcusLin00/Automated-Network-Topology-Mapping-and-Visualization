4616134c689c2d0fc12bfd89c736100569679873
Malcolm <malcolm.limhw@gmail.com>
2024-11-09 16:42:02 +0800

    fixed typos

--- client/monitors/__init__.py
+++ client/monitors/__init__.py
@@ -1,6 +1,6 @@
 # monitors/__init__.py
 from .base_monitor import BaseMonitor
 from .port_scan_monitor import PortScanMonitor
-from .device_healt_monitor import DeviceHealthMonitor
+from .device_health_monitor import DeviceHealthMonitor
 
-__all__ = ["BaseMonitor", "PortScanMonitor, DeviceHealthMonitor"]
+__all__ = ["BaseMonitor", "PortScanMonitor", "DeviceHealthMonitor"]
